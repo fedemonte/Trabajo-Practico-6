eafcc64c9a5fa0f54fec6cfc92e4f49b2c2bd50f
marianortiz <mariano2073@gmail.com>
2021-10-19 19:30:57 -0300

    Update Entities
    Se agregó un nuevo atributo a la entidad "Producto".

--- src/main/java/entities/Producto.java
+++ src/main/java/entities/Producto.java
@@ -1,7 +1,8 @@
 package entities;
 
 public class Producto {
-
+	
+	private int id;
     private String nombre;
     private String detalle;
     private Double precio;
@@ -13,15 +14,24 @@ public class Producto {
     }
 
 
-    public Producto(String nombre, String detalle, boolean disponible, double precio) {
+    public Producto(int id, String nombre, String detalle, boolean disponible, double precio) {
         super();
+        this.id = id;
         this.nombre = nombre;
         this.detalle = detalle;
         this.precio = precio;
         this.disponible = disponible;
     }
+    
+    
+    public int getId() {
+        return id;
+    }
 
-
+    public void setId(int id) {
+        this.id = id;
+    }
+    
     public String getNombre() {
         return nombre;
     }
--- src/test/java/test/CarritoComprasTest.java
+++ src/test/java/test/CarritoComprasTest.java
@@ -7,10 +7,10 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CarritoComprasTest {
     CarritoCompras carrito = new CarritoCompras();
-    Producto p1 = new Producto("IPhone","IPhone 12 Pro",true,1469.00);
-    Producto p3 = new Producto("Xiaomi","Redmi Note 8",false,629.00);
-    Producto p2 = new Producto("IPhone", "IPhone SE",true, 589.00);
-    Producto p4 = new Producto("Samsung","Samsung Galaxy M12s",true, 679.00);
+    Producto p1 = new Producto(1,"IPhone","IPhone 12 Pro",true,1469.00);
+    Producto p3 = new Producto(2,"Xiaomi","Redmi Note 8",false,629.00);
+    Producto p2 = new Producto(3,"IPhone", "IPhone SE",true, 589.00);
+    Producto p4 = new Producto(4,"Samsung","Samsung Galaxy M12s",true, 679.00);
     @org.junit.jupiter.api.Test
     void obtenerDescripcionProductos() {
         carrito.agregarProducto(p1);
