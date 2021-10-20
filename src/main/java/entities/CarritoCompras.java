4bfdb5481c064d8200f46373b16b3f34c642859c
marianortiz <mariano2073@gmail.com>
2021-10-19 21:29:50 -0300

    Update CarritoCompraTest
    Se agrego el test de productos ya existentes.

--- src/main/java/entities/CarritoCompras.java
+++ src/main/java/entities/CarritoCompras.java
@@ -6,8 +6,6 @@ public class CarritoCompras {
     private final ArrayList<Producto> productos = new ArrayList();
 
     public CarritoCompras() {
-
-        
     }
 
     public void agregarProducto(Producto p) {
@@ -22,4 +20,16 @@ public class CarritoCompras {
     public Integer verCantidadDeProductos() {
         return productos.size();
     }
+    
+    public Integer buscarProducto(Producto p) {
+    	int busqueda = 0;
+    	for (int i = 0; i < productos.size(); ++i) {
+    		if (p.getId() == productos.get(i).getId()) {
+    			busqueda = 1;
+    			break;
+    		}
+    	}
+    return busqueda;	
+    }
+    
 }
--- src/test/java/test/CarritoComprasTest.java
+++ src/test/java/test/CarritoComprasTest.java
@@ -6,11 +6,14 @@ import entities.Producto;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CarritoComprasTest {
+	
     CarritoCompras carrito = new CarritoCompras();
     Producto p1 = new Producto(1,"IPhone","IPhone 12 Pro",true,1469.00);
     Producto p3 = new Producto(2,"Xiaomi","Redmi Note 8",false,629.00);
     Producto p2 = new Producto(3,"IPhone", "IPhone SE",true, 589.00);
     Producto p4 = new Producto(4,"Samsung","Samsung Galaxy M12s",true, 679.00);
+    
+    
     @org.junit.jupiter.api.Test
     void obtenerDescripcionProductos() {
         carrito.agregarProducto(p1);
@@ -26,4 +29,16 @@ class CarritoComprasTest {
         String res_esperado = "IPhone 12 Pro";
         assertEquals(res_esperado,res_obtenido);
     }
+    
+    @org.junit.jupiter.api.Test
+    void agregarProdutoYaExistenteAlCarritoDeCompras() {
+    	carrito.agregarProducto(p1);
+    	carrito.agregarProducto(p2);
+    	carrito.agregarProducto(p3);
+    	int res_obtenido = carrito.buscarProducto(p1);
+    	int res_esperado = 1;
+    	assertEquals(res_obtenido, res_esperado);
+    }
+    
+    
 }
