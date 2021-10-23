e134d9d1176229f6c3f2af0fc6c13f7be35e4301
SantiagoBullaude <santiagobullaude@gmail.com>
2021-10-23 19:13:59 -0300

    Clase Producto modificada

--- src/test/java/test/CarritoComprasTest.java
+++ src/test/java/test/CarritoComprasTest.java
@@ -12,7 +12,7 @@ class CarritoComprasTest {
     Producto p3 = new Producto(2,"Xiaomi","Redmi Note 8",false,629.00);
     Producto p2 = new Producto(3,"IPhone", "IPhone SE",true, 589.00);
     Producto p4 = new Producto(4,"Samsung","Samsung Galaxy M12s",true, 679.00);
-    
+    Producto p5 = new Producto(4,"Samsung","Samsung Galaxy S20",true, 754.00);
     
     @org.junit.jupiter.api.Test
     void  verCantidadDeProductos(){
@@ -49,4 +49,13 @@ class CarritoComprasTest {
     	}
     }
     
+    @org.junit.jupiter.api.Test
+    public void agregar_producto__al_carrito_afecta_stock() {
+    	carrito.agregarProducto(p5);
+    	assertEquals(54,p5.getstockDisponible());
+    	
+    	
+    }
+    
+
 }
